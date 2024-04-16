ec25cfdda55f9226ed5d9d3c9c4ef8592c5ff68d
Théodore <theodore.deconinck@gmail.com>
2024-04-16 15:03:03 +0200

    add something

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <motion.div className="text-white flex justify-center"
         >
-            <span className="text-xs pt-5 pb-1 underline">© 2024 <a className="hover:text-purple-400" href="https://www.instagram.com/ovbmova/">OVB</a>, Inc. Tous droits réservés</span>
+            <span className="text-xs pt-5 pb-1 underline">© 2024 <a className="hover:text-purple-400" href="https://www.instagram.com/ihateovb/">OVB</a>, Inc. Tous droits réservés</span>
             
         </motion.div>
     )
